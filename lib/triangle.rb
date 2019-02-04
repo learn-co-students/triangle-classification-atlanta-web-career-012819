98d02e911f267cc6626f3f76f1a1f73e99ab720a
Dana Simmons <dana@simmons-audio.com>
2019-02-04 14:42:01 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,25 @@
+require 'pry'
 class Triangle
+  attr_reader :a,:b,:c,:kind
+
+  def initialize(*args)
+    sides = args.sort
+    if sides[0] + sides[1] <= sides[2]
+      raise TriangleError
+    else
+      a,b,c = sides
+      binding.pry
+      if a == c && a == b
+        @kind = :equilateral
+      elsif a != b && b == c
+        @kind = :isosceles
+      elsif a != b && b != c
+        @kind = :scalene
+      end
+    end
+  end
+
+  class TriangleError < StandardError
+  end
   # write code here
 end
